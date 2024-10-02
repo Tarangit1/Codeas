74af41ccd55d3de5e57afefe15a0fdbc8ddf4d06
Tarangit1 <106868648+Tarangit1@users.noreply.github.com>
2024-10-02 16:35:25 +0000

    done

--- CPP/cpp_class.cpp
+++
@@ -1,8 +0,0 @@
-/*
-Create a class Counter with a static member variable count 
-and a static member function incrementCount() that increments count by 1.
-Add another static member function getCount() that
-returns the current value of count.Demonstrate its usage in main().*/
-
-
-#include<iostream>
